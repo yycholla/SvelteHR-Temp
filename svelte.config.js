caaa8eeb1f09fe1b9a68e8f61da53e9331b8df37
chanway <chanway@hey.com>
2025-12-09 22:55:11 -0700

    fix: Add $routes alias to svelte.config.js for test imports
    Fixes missing module error in trainings create page test by adding
    the $routes path alias configuration. This allows test files to
    import route components using the $routes/ prefix.

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,10 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 
+		alias: {
+			$routes: './src/routes'
+		},
+
 		experimental: {
 			tracing: {
 				server: true
